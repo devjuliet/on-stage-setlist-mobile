4b34202728897668b5d8609445d4073f159cb303
Raul-DA <a315947@uach.mx>
2020-05-25 01:44:56 -0600

    App Ajustes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
   {
     path: 'profile',
     loadChildren: () => import('./views/profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'options',
+    loadChildren: () => import('./views/options/options.module').then( m => m.OptionsPageModule)
   }
 
 ];
