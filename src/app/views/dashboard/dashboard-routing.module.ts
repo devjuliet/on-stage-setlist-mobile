5126cdb9420825b5423d209ce81d428683fe5c73
Luismiguel Ortiz <luismi.luu@gmail.com>
2020-05-27 00:28:46 -0600

    Creacion del router, el menu y algunas vistas que faltaban junto con el acomodo conrrecto de las paginas

---
+++ src/app/views/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { DashboardPage } from './dashboard.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
+      },
+      {
+        path: 'sets-lists',
+        loadChildren: () => import('./playlist/playlist.module').then(m => m.PlaylistPageModule)
+      },
+      {
+        path: 'settings',
+        loadChildren: () => import('./options/options.module').then(m => m.OptionsPageModule)
+      },
+      {
+        path: 'list/:id',
+        loadChildren: () => import('./songlist/songlist.module').then(m => m.SonglistPageModule)
+      },
+      {
+        path: 'song/:id',
+        loadChildren: () => import('./song/song.module').then(m => m.SongPageModule)
+      },
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'song',
+    loadChildren: () => import('./song/song.module').then( m => m.SongPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class DashboardPageRoutingModule {}
