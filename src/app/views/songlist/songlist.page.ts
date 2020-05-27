0a790ed581a3635ceee852604cba0bd1322dde2a
Raúl Díaz <a315947@uach.mx>
2020-05-26 18:48:15 -0600

    Playlst and song list

---
+++ src/app/views/songlist/songlist.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-songlist',
+  templateUrl: './songlist.page.html',
+  styleUrls: ['./songlist.page.scss'],
+})
+export class SonglistPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
